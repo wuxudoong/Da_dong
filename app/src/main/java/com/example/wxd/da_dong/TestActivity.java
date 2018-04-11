f3332fd3ac1c539adee359d64f4b100e29e33002
DaDong <dadong@DaDongdeMacBook-Pro.local>
2018-04-11 23:32:26 +0800

    ARouter

--- app/src/main/java/com/example/wxd/da_dong/TestActivity.java
+++ app/src/main/java/com/example/wxd/da_dong/TestActivity.java
@@ -1,10 +1,12 @@
 package com.example.wxd.da_dong;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.widget.Button;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.example.wxd.da_dong.databinding.ActivityMainBinding;
 import com.router.ActivityRouter;
 
 /**
@@ -15,10 +17,11 @@ import com.router.ActivityRouter;
 @Route(path = ActivityRouter.APP_MAIN)
 public class TestActivity extends Activity {
 
+    ActivityMainBinding mBinding;
     @Override
     public void  onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         Button btnTest = findViewById(R.id.btn_test);
         btnTest.setOnClickListener(v -> ActivityRouter.gotoMemberLoginActivity(this));
     }
--- dadonglib/src/main/java/com/DaDongApplication.java
+++ dadonglib/src/main/java/com/DaDongApplication.java
@@ -1,16 +1,30 @@
 package com;
 
-import com.base.BaseApplication;
+import android.app.Application;
+import android.content.Context;
 
-import me.chunsheng.logboy.BuildConfig;
+import com.alibaba.android.arouter.launcher.ARouter;
 
 /**
  * Created by dadong on 2018/4/10.
  */
 
-public class DaDongApplication extends BaseApplication {
+public class DaDongApplication extends Application {
+
+
+    public static Context context;
+
+    public static Context getContext() {
+        return context;
+    }
+
     @Override
-    protected boolean debug() {
-        return BuildConfig.DEBUG;
+    public void onCreate() {
+        super.onCreate();
+        context = this;
+        ARouter.openLog();     // 打印日志
+        ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+
+        ARouter.init(this);
     }
 }
